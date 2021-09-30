85f2defac57b317899a2467f45bc1153a5887756
igor-lourenco <igor.lourencosantos@gmail.com>
2021-09-29 23:03:21 -0300

    GET paged, POST, PUT, DELETE Musica

--- backend/src/main/java/com/spring/artistas/resources/MusicaResource.java
+++ backend/src/main/java/com/spring/artistas/resources/MusicaResource.java
@@ -1,11 +1,20 @@
 package com.spring.artistas.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.spring.artistas.dto.MusicaDTO;
 import com.spring.artistas.services.MusicaService;
@@ -17,10 +26,36 @@ public class MusicaResource {
 	@Autowired
 	private MusicaService service;
 	
+	@GetMapping
+	public ResponseEntity<Page<MusicaDTO>> findAllPaged(Pageable pageable) {
+		Page<MusicaDTO> entity = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(entity);
+	}
+	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<MusicaDTO> findById(@PathVariable Integer id) {	
+	public ResponseEntity<MusicaDTO> findById(@PathVariable Integer id) {
 		MusicaDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<MusicaDTO> insert(@RequestBody MusicaDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(dto.getId()).toUri();  //mostra no cabeçalho da resposta o endereço da entidade criada
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<MusicaDTO> update(@PathVariable Integer id, @RequestBody MusicaDTO dto) {
+		MusicaDTO entity = service.update(id, dto);	
+		return ResponseEntity.ok().body(entity);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<MusicaDTO> delete(@PathVariable Integer id) {
+		 service.delete(id);	
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/com/spring/artistas/services/MusicaService.java
+++ backend/src/main/java/com/spring/artistas/services/MusicaService.java
@@ -2,13 +2,23 @@ package com.spring.artistas.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.spring.artistas.dto.ArtistaDTO;
 import com.spring.artistas.dto.MusicaDTO;
+import com.spring.artistas.entities.Artista;
 import com.spring.artistas.entities.Musica;
+import com.spring.artistas.repositories.ArtistaRepository;
 import com.spring.artistas.repositories.MusicaRepository;
+import com.spring.artistas.services.exception.DatabaseException;
 import com.spring.artistas.services.exception.ResourceNotFoundException;
 
 @Service
@@ -17,11 +27,61 @@ public class MusicaService {
 	@Autowired
 	private MusicaRepository repository;
 	
+	@Autowired
+	private ArtistaRepository artistaRepository;
+	
 	@Transactional(readOnly = true)
 	public MusicaDTO findById(Integer id) {
 		Optional<Musica> obj = repository.findById(id);
-		Musica entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado !!" + id));
+		Musica entity = obj.orElseThrow(() -> new ResourceNotFoundException("Musica não encontrado -> " + id));
 		return new MusicaDTO(entity, entity.getAutores());
+	}
+
+	@Transactional(readOnly = true)
+	public Page<MusicaDTO> findAllPaged(Pageable pageable) {
+		Page<Musica> entity = repository.findAll(pageable);
+		return entity.map(x -> new MusicaDTO(x, x.getAutores()));
+	}
+
+	@Transactional()
+	public MusicaDTO insert(MusicaDTO dto) {
+		Musica entity = new Musica();
+		copyDtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return new MusicaDTO(entity);
+	}
+	
+	public void  delete(Integer id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("id não existe " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade no banco");
+		}
+	}
+	
+	@Transactional()
+	public MusicaDTO update(Integer id, MusicaDTO dto) {
+		try {
+			Musica entity = repository.getOne(id);
+			copyDtoEntity(dto, entity);
+			entity = repository.save(entity);
+			return new MusicaDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id não encontrado " + id);
+		}
+	}
+
+	private void copyDtoEntity(MusicaDTO dto, Musica entity) {
+		entity.setNome(dto.getNome());
+		entity.setDuracao(dto.getDuracao());
+
+		entity.getAutores().clear();
+		for (ArtistaDTO artistaDto : dto.getAutores()) {
+			Artista artista = artistaRepository.getOne(artistaDto.getId());
+			entity.getAutores().add(artista);
+		}
 	}
 
 }
